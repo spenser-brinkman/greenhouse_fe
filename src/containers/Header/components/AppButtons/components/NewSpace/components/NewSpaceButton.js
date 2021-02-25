fd2ceeb8e07e9970a071ceeed8caa8ecdd1bd99f
spenser6131 <brinkman.spenser@gmail.com>
2021-02-24 19:25:57 -0800

    basic new space button component

---
+++ src/containers/Header/components/AppButtons/components/NewSpace/components/NewSpaceButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewPlantForm extends Component {
+  render(){
+    return (
+      <button id="door-button">
+        <div id="door-icon" class="green-door"></div>
+      </button>
+    )
+  }
+}
+
+export default NewPlantForm
