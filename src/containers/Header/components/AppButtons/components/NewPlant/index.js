0184d0659f35740f7fe72f1a3e55cd76db6bea85
spenser6131 <brinkman.spenser@gmail.com>
2021-02-24 19:25:20 -0800

    basic new plant component

--- src/containers/Header/components/AppButtons/components/NewPlant/index.js
+++ src/containers/Header/components/AppButtons/components/NewPlant/index.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+
+import NewPlantButton from './components/NewPlantButton'
+import NewPlantForm from './components/NewPlantForm'
+
 import './styles.scss'
 
 class NewPlant extends Component {
   render(){
     return (
       <div>
-        NewPlant
+        <NewPlantButton />
+        <NewPlantForm />
       </div>
     )
   }
