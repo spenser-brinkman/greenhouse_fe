2e8a8f09e6467b27c7a85e57a485838e2de09712
spenser6131 <brinkman.spenser@gmail.com>
2021-02-24 17:23:06 -0800

    main header container

---
+++ src/containers/Header/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
+import AppButtons from './components/AppButtons/'
+import Logo from './components/Logo/'
+import Menu from './components/Menu/'
+import './styles.scss'
+
+
+class Header extends Component {
+  render(){
+    return (
+      <>
+        <div id="header">
+          <AppButtons />
+          <Logo />
+          <Menu />
+        </div>
+        <hr className="line" />
+      </>
+    )
+  }
+}
+
+export default Header
