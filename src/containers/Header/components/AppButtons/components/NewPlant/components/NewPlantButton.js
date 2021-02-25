ea1960dfcfba239ea49e63db0927e759cf1c4604
spenser6131 <brinkman.spenser@gmail.com>
2021-02-24 19:25:37 -0800

    basic new plant button component

---
+++ src/containers/Header/components/AppButtons/components/NewPlant/components/NewPlantButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewPlantButton extends Component {
+  render(){
+    return (
+      <button id="leaf-button">
+        <div id="leaf-icon" class="green-leaf"></div>
+      </button>
+    )
+  }
+}
+
+export default NewPlantButton
