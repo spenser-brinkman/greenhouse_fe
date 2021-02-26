658b25e48012c9493905883a8eae5930f9a93693
spenser6131 <brinkman.spenser@gmail.com>
2021-02-26 13:27:12 -0800

    fully functioning forms

--- src/containers/Header/components/AppButtons/components/NewPlantForm.js
+++ src/containers/Header/components/AppButtons/components/NewPlantForm.js
@@ -2,49 +2,76 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 
 import { closeDropdown } from '../../../../../services/actions/headerActions'
+import { createPlant } from '../../../../../services/actions/plantActions'
+
+const defaultState = {
+  species: '',
+  lightReq: '',
+  humidityReq: '',
+  waterFreq: '',
+  lastWater: '',
+  lastFert: '',
+  comments: ''
+}
 
 class NewPlantForm extends Component {
+
+  state = defaultState
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.createPlant(this.state);
+    this.setState(defaultState);
+    this.props.closeDropdown();
+  }
+
   render(){
     return (
       <div className={this.props.showPlantForm ? "menu" : "menu hidden"} >
-        <form onSubmit={this.props.closeNewPlant} id="new-plant-form" className="dropdown-form">
+        <form onSubmit={this.handleSubmit} id="new-plant-form" className="dropdown-form">
           
           <h2>New Plant</h2>
           <br />
           
           <label>
             <h3>Species:</h3>
-            <input type="text" name="species" value="" />
+            <input type="text" name="species" value={this.state.species} onChange={this.handleChange} />
           </label>
 
           <label>
-            <h3>Light Requirement:</h3>
-            <input type="text" name="light_requirement" value="" />
+            <h3>Light Requirement (1-5):</h3>
+            <input type="text" name="lightReq" value={this.state.lightReq} onChange={this.handleChange} />
           </label>
 
           <label>
-            <h3>Humidity Requirement:</h3>
-            <input type="text" name="humidity_requirement" value="" />
+            <h3>Humidity Requirement (1-5):</h3>
+            <input type="text" name="humidityReq" value={this.state.humidityReq} onChange={this.handleChange} />
           </label>
 
           <label>
-            <h3>Watering Frequency:</h3>
-            <input type="text" name="water_frequency" value="" />
+            <h3>Watering Frequency (every "__" days):</h3>
+            <input type="text" name="waterFreq" value={this.state.waterFreq} onChange={this.handleChange} />
           </label>
 
           <label>
             <h3>Date Last Watered:</h3>
-            <input type="date" name="last_watering" value="" />
+            <input type="date" name="lastWater" value={this.state.lastWater} onChange={this.handleChange} />
           </label>
 
           <label>
             <h3>Date Last Fertilized:</h3>
-            <input type="date" name="last_fertilization" value="" />
+            <input type="date" name="lastFert" value={this.state.lastFert} onChange={this.handleChange} />
           </label>
 
           <label>
             <h3>Comments:</h3>
-            <input type="text" name="comments" value="" />
+            <input type="text" name="comments" value={this.state.comments} onChange={this.handleChange} />
           </label>
 
           <label>
@@ -64,7 +91,8 @@ const mstp = state => {
 
 const mdtp = dispatch => {
   return {
-    closeNewPlant: () => dispatch(closeDropdown)
+    closeDropdown: () => dispatch(closeDropdown),
+    createPlant: body => dispatch(createPlant(body))
   }
 }
 
--- src/containers/Header/components/AppButtons/components/NewSpaceForm.js
+++ src/containers/Header/components/AppButtons/components/NewSpaceForm.js
@@ -2,32 +2,57 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 
 import { closeDropdown } from '../../../../../services/actions/headerActions'
+import { createSpace } from '../../../../../services/actions/spaceActions'
+
+const defaultState = {
+  name: '',
+  light: '',
+  humidity: '',
+}
 
 class NewSpaceForm extends Component {
+  
+  state = defaultState
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.createSpace(this.state);
+    this.setState(defaultState);
+    this.props.closeDropdown();
+  }
+
   render(){
     return (
       <div className={this.props.showSpaceForm ? "menu" : "menu hidden"} >
-        <form onSubmit={this.props.closeNewSpace} id="new-space-form" className="dropdown-form">
-          <h2>New Space</h2>
-            <br />
-            <label>
-              <h3>Name:</h3>
-              <input type="text" name="name" value="" />
-            </label>
-            
-            <label>
-              <h3>Humidity Level:</h3>
-              <input type="text" name="humidity" value="" />
-            </label>
-            
-            <label>
-              <h3>Light Level:</h3>
-              <input type="text" name="light" value="" />
-            </label>
+        <form onSubmit={this.handleSubmit} id="new-space-form" className="dropdown-form">
 
-            <label>
-              <input type="submit" value="Create" />
-            </label>
+          <h2>New Space</h2>
+          <br />
+
+          <label>
+            <h3>Name:</h3>
+            <input type="text" name="name" value={this.state.name} onChange={this.handleChange} />
+          </label>
+          
+          <label>
+            <h3>Light Level (1-5):</h3>
+            <input type="text" name="light" value={this.state.light} onChange={this.handleChange} />
+          </label>
+          
+          <label>
+            <h3>Humidity Level (1-5):</h3>
+            <input type="text" name="humidity" value={this.state.humidity} onChange={this.handleChange} />
+          </label>
+
+          <label>
+            <input type="submit" value="Create" onChange={this.handleChange} />
+          </label>
         </form>
       </div>
     )
@@ -42,7 +67,8 @@ const mstp = state => {
 
 const mdtp = dispatch => {
   return {
-    closeNewSpace: () => dispatch(closeDropdown)
+    closeDropdown: () => dispatch(closeDropdown),
+    createSpace: body => dispatch(createSpace(body))
   }
 }
 
