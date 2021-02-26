dc339d1b644dc426f424145a188a2421a8de19f4
spenser6131 <brinkman.spenser@gmail.com>
2021-02-25 19:37:40 -0800

    little reference sheet

---
+++ z useful snippets.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import { connect } from 'react-redux'
+
+import './styles.scss'
+
+const mstp = state => {
+  return {
+
+  }
+}
+
+const mdtp = dispatch => {
+  return {
+
+  }
+}
