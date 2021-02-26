e116a2b34a1e7a14648209d49549ea8c740f95e0
spenser6131 <brinkman.spenser@gmail.com>
2021-02-26 13:27:28 -0800

    keyboard accessibility fix

--- src/containers/Header/components/Menu/components/MenuButton/index.js
+++ src/containers/Header/components/Menu/components/MenuButton/index.js
@@ -3,14 +3,17 @@ import React, { Component } from 'react';
 import './styles.scss'
 
 class MenuButton extends Component {
+
   render(){
     return (
       <button id="menu-button">
-        <div
+        <img
+          tabIndex="0"
+          onKeyPress={event => event.code === "Enter" ? this.props.onClick() : null }
           onClick={this.props.onClick}
           id="burger-icon"
-          className={this.props.color === 'green' ? "green-burger" : "orange-burger"}
-        ></div>
+          className={this.props.color === 'green' ? 'green-burger' : 'orange-burger'} 
+        />
       </button>
     )
   }
