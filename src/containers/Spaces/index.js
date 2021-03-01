25e52064020819305cfbcb6223b29c6f09533475
spenser6131 <brinkman.spenser@gmail.com>
2021-03-01 13:54:04 -0800

    spaces section framework

---
+++ src/containers/Spaces/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+import { connect } from 'react-redux'
+
+import './styles.scss'
+
+class Spaces extends Component {
+
+  render() {
+    return (
+      <>
+        SPACES
+      </>
+    )
+  }
+}
+
+const mstp = state => {
+  return {
+
+  }
+}
+
+const mdtp = dispatch => {
+  return {
+
+  }
+}
+
+export default Spaces
