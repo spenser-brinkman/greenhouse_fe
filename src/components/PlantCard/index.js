ed30086e5b857c59fa74d6e203ad9108f413468c
spenser6131 <brinkman.spenser@gmail.com>
2021-03-02 14:03:32 -0800

    restructure

--- src/components/PlantCard/index.js
+++ src/components/PlantCard/index.js
@@ -6,26 +6,12 @@ import PlantInfo from './components/PlantInfo'
 import './styles.scss'
 
 class PlantCard extends Component {
-  
-  state = {
-    open: false
-  }
-
-  handleClick = () => {
-    this.setState({
-      open: !this.state.open
-    })
-  }
 
   render() {
-    let plant = this.props.plant
     return (
       <div className="plant-card">
         <div className="holder card-content">
-          <ul className="plant">
-            <p onClick={this.handleClick}>{this.props.plant.species}</p>
-            {this.state.open && <PlantInfo plant={plant} />}
-          </ul>
+          <PlantInfo plant={this.props.plant} />
         </div>
       </div>
     )
