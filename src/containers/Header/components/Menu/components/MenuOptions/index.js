a3933eec5fcd89f63d19884fa141d1a05336f518
spenser6131 <brinkman.spenser@gmail.com>
2021-03-01 20:18:42 -0800

    temp comment

--- src/containers/Header/components/Menu/components/MenuOptions/index.js
+++ src/containers/Header/components/Menu/components/MenuOptions/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 // import Login from './components/Login/'
 // import Signup from './components/Signup/'
-import About from './components/About'
+// import About from './components/About'
 
 import './styles.scss'
 
