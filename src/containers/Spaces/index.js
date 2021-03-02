f98ee42bbaf7aa59ca17ec60d9f082c1d657b49b
spenser6131 <brinkman.spenser@gmail.com>
2021-03-01 20:21:36 -0800

    building spaces section

--- src/containers/Spaces/index.js
+++ src/containers/Spaces/index.js
@@ -1,30 +1,42 @@
 import React, { Component } from 'react';
-
 import { connect } from 'react-redux'
+
+import { fetchSpaces } from '../../services/actions/spaceActions'
+import SpaceCard from '../../components/SpaceCard/'
 
 import './styles.scss'
 
 class Spaces extends Component {
 
+  componentDidMount(){
+    this.props.fetchSpaces()
+  }
+
   render() {
+    
+    console.log(this.props);
+    console.log('*');
+    console.log('*');
+    console.log('*');
+    console.log('*');
     return (
-      <>
-        SPACES
-      </>
+      <div className="right">
+        {this.props.spaces.map(space => <SpaceCard space={space.attributes} key={space.id} />)}
+      </div>
     )
   }
 }
 
 const mstp = state => {
   return {
-
+    spaces: state.spaces.data
   }
 }
 
 const mdtp = dispatch => {
   return {
-
+    fetchSpaces: () => dispatch(fetchSpaces())
   }
 }
 
-export default Spaces
+export default connect(mstp, mdtp)(Spaces)
