f790761424d805efed3b242a72b7c1ba78f7d2d6
spenser6131 <brinkman.spenser@gmail.com>
2021-03-09 14:05:29 -0800

    basic login form

--- src/containers/Header/components/Menu/components/MenuOptions/components/Login.js
+++ src/containers/Header/components/Menu/components/MenuOptions/components/Login.js
@@ -1,14 +1,57 @@
 import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux'
 
+import { login } from '../../../../../../../services/actions/userActions'
 
 class Login extends Component {
+
+  state = {
+    username: '',
+    password: ''
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    this.props.login(this.state)
+  }
+
   render(){
     return (
-      <div>
-        LOGIN!
-      </div>
+      <>
+        <form className="dropdown-form" onSubmit={this.handleSubmit}>
+
+          <h3>Username:</h3>
+          <input type="text" name="username" value={this.state.username} onChange={this.handleChange} />
+
+          <h3>Password:</h3>
+          <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
+
+          <input type="submit" value="Log In" />
+        </form>
+
+        <NavLink to="/menu" />
+      </>
     )
   }
 }
 
-export default Login
+const mstp = state => {
+  return {
+
+  }
+}
+
+const mdtp = dispatch => {
+  return {
+    login: user => dispatch(login(user))
+  }
+}
+
+export default connect(null, mdtp)(Login)
