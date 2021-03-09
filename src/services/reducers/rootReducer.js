eacaea30bab9bba3ce2ff7d7b4e724b44aa53a3f
spenser6131 <brinkman.spenser@gmail.com>
2021-03-09 14:06:57 -0800

    add user reducer

--- src/services/reducers/rootReducer.js
+++ src/services/reducers/rootReducer.js
@@ -3,8 +3,10 @@ import { combineReducers } from 'redux';
 import plantsReducer from './plantsReducer';
 import spacesReducer from './spacesReducer';
 import headerReducer from './headerReducer';
+import userReducer from './userReducer';
 
 const rootReducer = combineReducers({
+  user: userReducer,
   plants: plantsReducer,
   spaces: spacesReducer,
   header: headerReducer
