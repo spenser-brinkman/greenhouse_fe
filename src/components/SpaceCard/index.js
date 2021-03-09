91c1c5ad98ad91b04142a1196e127d68cb044ffe
spenser6131 <brinkman.spenser@gmail.com>
2021-03-09 14:03:50 -0800

    functional edit/delete space

--- src/components/SpaceCard/index.js
+++ src/components/SpaceCard/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 
+import { editSpace, deleteSpace } from '../../services/actions/spaceActions'
 import { editPlant } from '../../services/actions/plantActions'
 
 import PlantInfo from '../PlantCard/components/PlantInfo'
@@ -9,15 +10,25 @@ import './styles.scss'
 
 class SpaceCard extends Component {
 
+  space = this.props.space
+
+  plants = this.props.plants.filter(plant => plant.attributes.spaceId === this.props.space.id)
+
   state = {
     spaceOpen: false,
-    spaceEditorOpen: false
+    spaceEditorOpen: false,
+    spaceEditFields: {
+      id: this.props.space.id,
+      name: this.props.space.name,
+      light: this.props.space.light,
+      humidity: this.props.space.humidity
+    }
   }
 
   drop = event => {
     event.preventDefault();
     const plant = JSON.parse(event.dataTransfer.getData("plant"))
-    plant.spaceId = this.props.space.id
+    plant.spaceId = this.space.id
     this.props.editPlant(plant)
   }
 
@@ -40,22 +51,58 @@ class SpaceCard extends Component {
   
   spaceDetails = () => {
     return (
-      <p>
-        Light: <span style={{fontWeight: 600}}>{this.props.space.light}</span>
-        &nbsp;| Humidity: <span style={{fontWeight: 600}}>{this.props.space.humidity}</span>
-      </p>
+      <>
+        <p>
+          Light: <span style={{fontWeight: 600}}>{this.space.light}</span>
+          &nbsp;| Humidity: <span style={{fontWeight: 600}}>{this.space.humidity}</span>
+        </p>
+        <button onClick={this.toggleEditForm}>Edit Space</button>
+      </>
+    )
+  }
+
+  handleFieldChange = event => {
+    this.setState({
+      spaceEditFields: {...this.state.spaceEditFields, [event.target.name]: event.target.value}
+    })
+  }
+
+  submitEditSpace = event => {
+    event.preventDefault();
+    this.props.editSpace(this.state.spaceEditFields);
+    this.toggleEditForm()
+  }
+
+  deleteSpace = event => {
+    event.preventDefault();
+    this.plants.map(plant => plant.attributes.spaceId = null && this.props.editPlant(plant))
+    this.props.deleteSpace(this.space.id, this.plants)
+  }
+
+  editSpaceForm = () => {
+    return (
+      <form onSubmit={this.submitEditSpace}>
+        <li>Light: <input type="text" name="light" value={this.state.spaceEditFields.light} onChange={this.handleFieldChange} /></li>
+        <li>Humidity: <input type="text" name="humidity" value={this.state.spaceEditFields.humidity} onChange={this.handleFieldChange} /></li>
+        <li>
+          <input type="submit" value="Submit" />
+          <button onClick={this.deleteSpace}>Delete</button>
+          <button onClick={this.toggleEditForm}>Cancel</button>
+        </li>
+      </form>
     )
   }
 
   render() {
-    let space = this.props.space
+    let plants = this.props.plants.filter(plant => plant.attributes.spaceId === this.props.space.id)
     return (
       <div className="space-card" onDrop={this.drop} onDragOver={this.dragOver}>
         <div className="holder card-content">
-          <h2 className={this.state.spaceOpen ? "open card-title" : "closed card-title"} onClick={this.toggleDetails}>{this.props.space.name}</h2>
+          <h2 className={this.state.spaceOpen ? "open card-title" : "closed card-title"} onClick={this.toggleDetails}>{this.space.name}</h2>
           {this.state.spaceOpen && this.spaceDetails()}
+          {this.state.spaceEditorOpen && this.editSpaceForm()}
           <div>
-            {this.props.plants.map(plant => plant.attributes.spaceId === space.id && <PlantInfo key={plant.attributes.id} plant={plant.attributes} />)}
+            {plants.map(plant => <PlantInfo key={plant.attributes.id} plant={plant.attributes} />)}
           </div>
         </div>
       </div>
@@ -71,7 +118,9 @@ const mstp = state => {
 
 const mdtp = dispatch => {
   return {
-    editPlant: plant => dispatch(editPlant(plant))
+    editPlant: plant => dispatch(editPlant(plant)),
+    editSpace: space => dispatch(editSpace(space)),
+    deleteSpace: (id, plants) => dispatch(deleteSpace(id, plants))
   }
 }
 
