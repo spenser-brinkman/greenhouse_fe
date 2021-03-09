ea367a3e0c9258fed5cb5ec3c5de9665fb808de4
spenser6131 <brinkman.spenser@gmail.com>
2021-03-09 14:05:37 -0800

    basic signup form

--- src/containers/Header/components/Menu/components/MenuOptions/components/Signup.js
+++ src/containers/Header/components/Menu/components/MenuOptions/components/Signup.js
@@ -1,14 +1,61 @@
 import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux'
 
+import { signup } from '../../../../../../../services/actions/userActions'
 
 class Signup extends Component {
+
+  state = {
+    username: '',
+    password: '',
+    passwordConfirmation: ''
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    this.props.signup(this.state)
+  }
+
   render(){
     return (
-      <div>
-        SIGNUP!
-      </div>
+      <>
+        <form className="dropdown-form" onSubmit={this.handleSubmit}>
+
+          <h3>Username:</h3>
+          <input type="text" name="username" value={this.state.username} onChange={this.handleChange} />
+
+          <h3>Password:</h3>
+          <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
+
+          <h3>Confirm Password:</h3>
+          <input type="password" name="passwordConfirmation" value={this.state.passwordConfirmation} onChange={this.handleChange} />
+
+          <input type="submit" value="Sign Up" />
+        </form>
+
+        <NavLink to="/menu" />
+      </>
     )
   }
 }
 
-export default Signup
+const mstp = state => {
+  return {
+
+  }
+}
+
+const mdtp = dispatch => {
+  return {
+    signup: user => dispatch(signup(user))
+  }
+}
+
+export default connect(null, mdtp)(Signup)
