086fe2e8358abcece0f8394e1525417d2b18bdb0
spenser6131 <brinkman.spenser@gmail.com>
2021-03-10 12:33:24 -0800

    improved app startup, allow user login/out

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
 import Header from './containers/Header/'
 import UnassignedPlants from './containers/UnassignedPlants/'
 import Spaces from './containers/Spaces/'
+import { fetchLoggedInUser } from './services/actions/userActions';
+import { fetchSpaces } from './services/actions/spaceActions'
+import { fetchPlants } from './services/actions/plantActions'
+
 
 import './styles.scss'
 
 class App extends Component {
+
+  componentDidMount(){
+    this.fetchEverything()   
+  }
+
+  fetchEverything = () =>{
+    this.props.fetchLoggedInUser()
+    this.props.fetchSpaces()
+    this.props.fetchPlants()
+  }
 
   render() {
     return (
@@ -20,5 +35,18 @@ class App extends Component {
   }
 }
 
+const mstp = state => {
+  return {
 
-export default App;
+  }
+}
+
+const mdtp = dispatch => {
+  return {
+    fetchLoggedInUser: () => dispatch(fetchLoggedInUser),
+    fetchSpaces: () => dispatch(fetchSpaces()),
+    fetchPlants: () => dispatch(fetchPlants())
+  }
+}
+
+export default connect(null, mdtp)(App);
