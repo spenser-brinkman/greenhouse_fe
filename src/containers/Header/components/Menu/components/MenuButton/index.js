b8c0e0ec0680da1e5a5b974d1b680a8f5300fd57
spenser6131 <brinkman.spenser@gmail.com>
2021-03-11 13:52:58 -0800

    minor routing

--- src/containers/Header/components/Menu/components/MenuButton/index.js
+++ src/containers/Header/components/Menu/components/MenuButton/index.js
@@ -1,26 +1,42 @@
 import React, { Component } from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux'
 
 import './styles.scss'
 
 class MenuButton extends Component {
 
+  burgerColor = () => this.props.color === 'green' ? 'green-burger' : 'orange-burger'
+
   render(){
     return (
-      <button id="menu-button">
-        <Link to="/menu">
+      <button id='menu-button'>
+        <Link to={this.props.showMenu ? '/' : '/menu'}>
           <img
-            tabIndex="0"
-            onKeyPress={event => event.code === "Enter" ? this.props.onClick() : null }
+            tabIndex='0'
+            onKeyPress={event => event.code === 'Enter' ? this.props.onClick() : null }
             onClick={this.props.onClick}
-            id="burger-icon"
-            className={this.props.color === 'green' ? 'green-burger' : 'orange-burger'} 
-            alt="Button To Open Menu"
+            id='burger-icon'
+            className={this.burgerColor()}
+            alt='Button To Open Menu'
           />
         </Link>
+
       </button>
     )
   }
 }
 
-export default MenuButton
+const mstp = state => {
+  return {
+    showMenu: state.header.showMenu
+  }
+}
+
+const mdtp = dispatch => {
+  return {
+
+  }
+}
+
+export default connect(mstp, mdtp)(MenuButton)
