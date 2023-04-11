169476660fcd69ef1f000a16c9d4d7e9045a9f26
spenser-brinkman <brinkman.spenser@gmail.com>
2023-04-11 16:17:11 -0700

    Reduce logging in production

--- src/services/middleware/logger.js
+++ src/services/middleware/logger.js
@@ -1,10 +1,12 @@
 const logger = store => next => action => {
-  console.group("action.type:", action.type)
-  console.info('dispatching action:', action)
-  let result = next(action)
-  console.log('next state:', store.getState())
-  console.groupEnd()
-  return result
+  if (process.env.NODE_ENV !== 'production') {
+    console.group("action.type:", action.type)
+    console.info('dispatching action:', action)
+    let result = next(action)
+    console.log('next state:', store.getState())
+    console.groupEnd()
+    return result
+  }
 }
 
 export default logger
